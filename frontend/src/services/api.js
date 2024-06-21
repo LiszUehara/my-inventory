b4a5f3db6a955b623330cf948b8a53141c202916
LiszUehara <ueharalisandra@gmail.com>
2024-06-21 16:26:54 -0300

    feat: start api integration

---
+++ frontend/src/services/api.js
@@ -0,0 +1,53 @@
+import env from '../utils/env';
+
+async function fetcher(url, options = {}) {
+    console.log(env.VITE_BACKEND_URL)
+  const response = await fetch(`${env.VITE_BACKEND_URL}${url}`, {
+    ...options,
+    headers: {
+      ...options.headers,
+      authorization: `Bearer ${sessionStorage.getItem('@token')}`,
+    },
+  });
+
+  const data = await response.json();
+
+  if (response.ok) {
+    return data;
+  }
+
+  const error = new Error();
+
+  error.cause = data.message;
+  error.original = data;
+
+  throw error;
+}
+
+fetcher.delete = function () {
+  return fetcher(url, {
+    method: 'DELETE',
+  });
+};
+
+fetcher.post = function (url, data) {
+  return fetcher(url, {
+    body: JSON.stringify(data),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+fetcher.put = function (url, data) {
+  return fetcher(url, {
+    body: JSON.stringify(data),
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export default fetcher;
