ba86f6bac0cece7ed8bd9c08b4b91e1331aea2d1
LiszUehara <ueharalisandra@gmail.com>
2024-06-21 15:56:05 -0300

    chore: remove unused code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import DisciplineContextProvider from "./context/discipline";
 import { Router } from "./routes";
 
 export default function App() {
 
   return (
     <div className="App">
-      <DisciplineContextProvider>
         <Router />
-      </DisciplineContextProvider>
     </div>
   );
 }
